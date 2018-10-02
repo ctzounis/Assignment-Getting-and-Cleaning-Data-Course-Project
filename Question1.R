45868ce756f3f7055afa7e7b78678c42cea54bc6
Christos Tzounis <christostzounis@Christoss-MacBook-Pro.local>
2018-10-01 21:54:52 -0700

    First commit

--- Question1.R
+++
@@ -1,5 +0,0 @@
-if(!file.exists("./data")){dir.create("./data")}
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
-download.file(fileUrl,destfile="./data/housing.csv",method="curl")
-hous <- read.csv("./data/housing.csv")
-strsplit(names(hous),"wgtp")
--- Question5.R
+++
@@ -1,6 +0,0 @@
-library(quantmod)
-amzn = getSymbols("AMZN",auto.assign=FALSE)
-sampleTimes = index(amzn)
-#create a table with the year and the weekdays of the sampleTimes
-#addmargins() is not necessary
-addmargins(table(year(sampleTimes), weekdays(sampleTimes)))
